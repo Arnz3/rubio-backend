21aab323802006787c9dfc1b7d687dc34ffd7e82
Arnz3 <cuyversarno@gmail.com>
2026-09-12 00:19:29 +0200

    add router

--- src/rubio_api/api/v1/__init__.py
+++ src/rubio_api/api/v1/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
 from rubio_api.api.v1.routers import organization
+from rubio_api.api.v1.routers import event
 
 router = APIRouter()
 
 router.include_router(organization.router)
+router.include_router(event.router)
